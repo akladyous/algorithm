24c6239a204db365ec6e2180c7aee7aafa7f6616
Boula Akladyous <akladyous@gmail.com>
2022-03-16 17:23:59 -0400

    two pointers - leetcode 26 Remove Duplicates from Sorted Array

--- binarySearch/finding-boundary/solution.rb
+++ binarySearch/finding-boundary/solution.rb
@@ -1,3 +1,16 @@
+=begin
+Finding Boundary with Binary Search
+Prereq: Binary Search Introduction
+
+An array of boolean values is divided into two sections; the left section consists of all false and the right section consists of all true. Find the boundary of the right section, i.e. the index of the first true element. If there is no true element, return -1.
+
+Input: arr = [false, false, true, true, true]
+
+Output: 2
+
+Explanation: first true's index is 2.
+
+=end
 
 def find_boundary(array)
     leftIndex, rightIndex = 0, array.length-1
