d974c149e0a571dbf5c0c363a05f8c53e623cf7f
Boula Akladyous <akladyous@gmail.com>
2022-03-29 18:55:40 -0400

    sliding-window logest-substring-without-repeating-characters

--- algo-monster/sliding-window/Longest Substring without Repeating Characters/solution.py
+++ algo-monster/sliding-window/Longest Substring without Repeating Characters/solution.py
@@ -1,17 +1,18 @@
 
-def lengthOfLongestSubstring(s: str):
+
+def longest_substring_without_repeating_characters(s: str) -> int:
     left = right = result = 0
     window = set()
     while right < len(s):
         if not s[right] in window:
             window.add(s[right])
             right += 1
+            # result = max(result, right - left)
             result = max(result, len(window))
         else:
             window.remove(s[left])
             left += 1
     return result
 
-print(lengthOfLongestSubstring("abcabcbb"))
-print(lengthOfLongestSubstring("aaaabaaa"))
-print(lengthOfLongestSubstring("abccabcabcc"))
+print(longest_substring_without_repeating_characters('abccabcabcc'))
+print(longest_substring_without_repeating_characters('aaaabaaa'))
