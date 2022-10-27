5b5387ac548d568f521f8ed529afcd4baa4a332a
Boula Akladyous <akladyous@gmail.com>
2022-10-27 11:49:53 -0400

    test

--- practice/get_string_middle.rb
+++ practice/get_string_middle.rb
@@ -1,13 +1,11 @@
-def get_middle string
-  quotient, reminder = string.length.divmod(2)
+def get_middle s
+  quotient, reminder = s.length.divmod(2)
   middle = reminder == 0 ? quotient-1..quotient : quotient
 
-  string[middle]
+  s[middle]
 
-  # string.length % 2 == 0 ? string.slice( string.length.div(2)-1..string.length.div(2) ) : string.slice(string.length.div(2))
+  # s.length % 2 == 0 ? s.slice( s.length.div(2)-1..s.length.div(2) ) : s.slice(s.length.div(2))
 
   # s.length % 2 != 0 ? s.split("")[~~(s.length / 2)] : s.split("")[s.length / 2 - 1] + s.split("")[s.length / 2]
 end
-
-
 
