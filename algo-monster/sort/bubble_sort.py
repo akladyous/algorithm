8e22a2477122e0281af9718c7a6608cf55849ee3
Boula Akladyous <akladyous@gmail.com>
2022-10-30 15:46:58 -0400

    py

--- algo-monster/sort/bubble_sort.py
+++ algo-monster/sort/bubble_sort.py
@@ -4,8 +4,7 @@ unsorted_list = [5, 3, 1, 2, 4, 8, 6, 9, 7, 10]
 
 
 def bubble_sort(unsorted_list: List[int]) -> List[int]:
-    n = len(unsorted_list)
-    for i in reversed(range(n)):
+    for i in reversed(range(len(unsorted_list))):
         swapped = False
         for j in range(i):
             if unsorted_list[j] > unsorted_list[j + 1]:
