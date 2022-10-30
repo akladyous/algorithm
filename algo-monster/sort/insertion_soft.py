35015d36cbf94f5d40b7d5fe9b7861403db433d9
Boula Akladyous <akladyous@gmail.com>
2022-10-30 13:14:23 -0400

    py

--- algo-monster/sort/insertion_soft.py
+++ algo-monster/sort/insertion_soft.py
@@ -1,12 +1,20 @@
 from typing import List
 
-l1 = [5, 3, 1, 2, 4]
+from numpy import sort
+
+unsorted_list = [5, 3, 1, 2, 4]
 
 
 def sort_list(unsorted_list: List[int]) -> List[int]:
-    for i, entry in enumerate(unsorted_list):
+    for i in range(len(unsorted_list)):
         current = i
         while current > 0 and unsorted_list[current] < unsorted_list[current - 1]:
-            unsorted_list[current], unsorted_list[current - 1] = unsorted_list[current - 1], unsorted_list[current]
+            unsorted_list[current - 1], unsorted_list[current] = unsorted_list[current], unsorted_list[current - 1]
             current -= 1
     return unsorted_list
+
+
+if __name__ == '__main__':
+    # unsorted_list = [int(x) for x in input().split()]
+    res = sort_list(unsorted_list)
+    print(' '.join(map(str, res)))
