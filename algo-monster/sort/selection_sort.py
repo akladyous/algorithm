0c561fb3ddd7687d190db5b172712e61377d29ad
Boula Akladyous <akladyous@gmail.com>
2022-10-30 14:35:23 -0400

    py

--- algo-monster/sort/selection_sort.py
+++ algo-monster/sort/selection_sort.py
@@ -10,13 +10,6 @@ if unsorted_list[j] < unsorted_list[min_index] -> swap
 
 
 def selection_sort(unsorted_list: list[int]) -> list[int]:
-    for i in range(len(unsorted_list)):
-        min_index = i
-        for j in range(i, len(unsorted_list)):
-            if unsorted_list[j] < unsorted_list[min_index]:
-                min_index = j
-        unsorted_list[i], unsorted_list[min_index] = unsorted_list[min_index], unsorted_list[i]
-    return unsorted_list
 
 
 if __name__ == '__main__':
