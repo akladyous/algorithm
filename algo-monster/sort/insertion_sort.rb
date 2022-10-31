885ed50d2f33cd98234603ab70fde29ec45a3c3a
Boula Akladyous <akladyous@gmail.com>
2022-10-31 00:50:03 -0400

    ruby insertion_sort_algo

--- algo-monster/sort/insertion_sort.rb
+++ algo-monster/sort/insertion_sort.rb
@@ -1,2 +1,13 @@
-l1=[1,2,3]
+unsorted_list = [5, 3, 1, 2, 4, 8, 6, 9, 7, 10]
 
+def insertion_sort(nums)
+  for i in 0...nums.length
+    current = i
+    while current > 0 && nums[current] < nums[current - 1]
+      nums[current - 1], nums[current] = nums[current], nums[current -1]
+      current -= 1
+    end
+  end
+  nums
+end
+p insertion_sort(unsorted_list)
