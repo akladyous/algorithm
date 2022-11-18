bd13ab240ba0b80b8505aa7516464d68b5dd6a36
Boula Akladyous <akladyous@gmail.com>
2022-11-18 09:06:45 -0500

    algo

--- practice/javascript/test2.js
+++ practice/javascript/test2.js
@@ -1,17 +1,4 @@
 function binarySearch(array = new Array, target) {
-    let left = 0
-    let right = array.length - 1
-    while (left <= right) {
-        let middle = Math.floor((left + right) / 2)
-        if (array[middle] == target) {
-            return target
-        };
-        if (array[middle] < target) {
-            left = middle + 1
-        } else {
-            right = middle - 1
-        }
-    };
-    return -1;
+
 };
 console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4));
