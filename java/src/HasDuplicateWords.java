b8d82c2a2521bf4d1110ffb7e74bfa3c092b9d89
Boula Akladyous <akladyous@gmail.com>
2023-03-02 01:37:22 -0500

    Untracked files on algorithm

--- java/src/HasDuplicateWords.java
+++ java/src/HasDuplicateWords.java
@@ -1,2 +1,17 @@
+import java.util.HashSet;
+
 public class HasDuplicateWords {
-}
+    public static void main(String[] args) {
+        Boolean result = hasDuplicateWords("ciao paolo come stai?");
+        System.out.println(result);
+    }
+    public static boolean hasDuplicateWords(String string) {
+        String[] words = string.split("\\s+");
+        HashSet<String> uniqueWords = new HashSet<>();
+        for (String word: words) {
+            if (uniqueWords.contains(word)) return true;
+            uniqueWords.add(word);
+        }
+        return true;
+    };
+};
