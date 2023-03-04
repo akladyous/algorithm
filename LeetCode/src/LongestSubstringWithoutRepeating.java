376f116d02b57cc60de9c5411e2f343504d105fe
Boula Akladyous <akladyous@gmail.com>
2023-03-03 19:47:00 -0500

    Untracked files on algorithm

---
+++ LeetCode/src/LongestSubstringWithoutRepeating.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+
+public class LongestSubstringWithoutRepeating {
+
+    public static void main(String[] args) {
+        int result = solution("abcabcbb");
+        System.out.println("solution : " + result);
+    }
+
+    public static int solution(String s) {
+       int left = 0, right = 0, max = 0;
+       HashSet<Character> window = new HashSet<> ();
+       while ( right < s.length() ) {
+           if (! window.contains(s.charAt(right))) {
+               window.add(s.charAt(right));
+               right ++;
+               max = Math.max(max, window.size());
+           } else {
+               window.remove(s.charAt(left));
+               left ++;
+           }
+       }
+       return max;
+    };
+
+}
+
