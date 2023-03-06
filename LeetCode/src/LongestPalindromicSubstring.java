61d12b1b4fd99dbafced386e40fbcd47645f6fd6
Boula Akladyous <akladyous@gmail.com>
2023-03-05 23:46:54 -0500

    Untracked files on algorithm

--- LeetCode/src/LongestPalindromicSubstring.java
+++ LeetCode/src/LongestPalindromicSubstring.java
@@ -1,14 +1,39 @@
 public class LongestPalindromicSubstring {
 
     public static void main(String[] args) {
+        System.out.println("longestPalindrome   a   : " + longestPalindrome("a"));
 //        System.out.println("isPalindrome   a   : " + isPalindrome("a"));
 //        System.out.println("isPalindrome   aa  : " + isPalindrome("aa"));
 //        System.out.println("isPalindrome   ab  : " + isPalindrome("ab"));
 //        System.out.println("isPalindrome   abc : " + isPalindrome("abc"));
 //        System.out.println("isPalindrome   aba : " + isPalindrome("aba"));
+//        System.out.println("isPalindrome   aba : " + isPalindrome("cbbd"));
         System.out.println("isPalindrome babad : " + isPalindrome("babad"));
 //        System.out.println("isPalindrome   bab : " + isPalindrome("bab"));
 //        System.out.println("isPalindrome  cbbd : " + isPalindrome("cbbd"));
+    }
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public static  int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
     }
     public static String isPalindrome(String s) {
         int left  = s.length() / 2, right = left;
