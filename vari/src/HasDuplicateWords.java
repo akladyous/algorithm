3abad73309a53045f049a9a03fbedb300e278cf2
Boula Akladyous <akladyous@gmail.com>
2023-03-16 11:44:07 -0400

    Untracked files on algorithm

--- vari/src/HasDuplicateWords.java
+++ vari/src/HasDuplicateWords.java
@@ -1,11 +1,13 @@
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class HasDuplicateWords {
     public static void main(String[] args) {
-        Boolean result = hasDuplicateWords("ciao paolo come stai?");
-        System.out.println(result);
+        System.out.println(" " + hasDuplicateWords_hashSet("apple pear melon apple pear apple"));
+        System.out.println(" " + hasDuplicateWords_hashSet("giraffe zebra giraffe"));
+        System.out.println(" " + hasDuplicateWords_hashSet("apple pear melon"));
     }
-    public static boolean hasDuplicateWords(String string) {
+    public static boolean hasDuplicateWords_hashSet(String string) {
         String[] words = string.split("\\s+");
         HashSet<String> uniqueWords = new HashSet<>();
         for (String word: words) {
@@ -14,4 +16,15 @@ public class HasDuplicateWords {
         }
         return true;
     };
+    public static HashMap<String, Integer> hasDuplicateWords(String string) {
+        HashMap<String, Integer> wordsCount = new HashMap<>();
+        for (String word : string.split("\\s+")) {
+            if (wordsCount.containsKey(word)) {
+                wordsCount.replace(word, wordsCount.get(word) + 1);
+            } else {
+                wordsCount.put(word, 1);
+            }
+        }
+        return wordsCount;
+    }
 };
