441327259ccfef937282a61ea54f8f9550371f27
Boula Akladyous <akladyous@gmail.com>
2023-03-22 19:25:51 -0400

    Untracked files on algorithm

--- vari/src/HasDuplicateChars.java
+++ vari/src/HasDuplicateChars.java
@@ -18,4 +18,16 @@ public class HasDuplicateChars {
         // If no duplicate characters were found, return false
         return false;
     }
+
+    public static void solution2(String s) {
+        // assuming all chars in s string are lowercase
+        int[] charCounts = new int[26];
+        for (char c : s.toCharArray()) {
+            charCounts[c - 'a'] ++;
+        }
+        // or
+        for (int i : s.getBytes()) {
+            charCounts[i - 97] ++ ;
+        }
+    }
 }
