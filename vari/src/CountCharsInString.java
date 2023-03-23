cbebd0008876d2bbd5e803a445865c3a052c1b6d
Boula Akladyous <akladyous@gmail.com>
2023-03-22 20:09:49 -0400

    Untracked files on algorithm

--- vari/src/CountCharsInString.java
+++ vari/src/CountCharsInString.java
@@ -1,3 +1,19 @@
 public class CountCharsInString {
 
+    public static void main(String[] args) {
+
+
+    }
+    public static void solution2(String s) {
+        // assuming all chars in s string are lowercase
+        int[] charCounts = new int[26];
+        for (char c : s.toCharArray()) {
+            charCounts[c - 'a'] ++;
+        }
+        // or
+        for (int i : s.getBytes()) {
+            charCounts[i - 97] ++ ;
+        }
+
+    }
 }
