2da1c26225f85b9050c2e2be14a27b8a48cc898b
Boula Akladyous <akladyous@gmail.com>
2023-03-28 12:58:54 -0400

    Untracked files on algorithm

--- vari/src/CountCharsInString.java
+++ vari/src/CountCharsInString.java
@@ -1,4 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountCharsInString {
+
+    public static void main(String[] args) {
+        solution1("aaaA");
+    }
 
 
     public static void solution1(String s) {
@@ -15,5 +22,15 @@ public class CountCharsInString {
             charCounts[c - 'a'] ++;
         };
     };
+
+
+    public static Map<Character, Integer> countCharacters(String input) {
+        Map<Character, Integer> charCounts = new HashMap<>();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            charCounts.put(c, charCounts.getOrDefault(c, 0) + 1);
+        }
+        return charCounts;
+    }
 
 }
