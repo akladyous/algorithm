7f2852cc1b17d70f938eceb9ba7efdb87b0b4cf3
Boula Akladyous <akladyous@gmail.com>
2023-03-28 16:19:00 -0400

    Untracked files on algorithm

--- vari/src/CountCharsInString.java
+++ vari/src/CountCharsInString.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -6,31 +7,32 @@ public class CountCharsInString {
 
     public static void main(String[] args) {
 //        solution1("aaaA");
-        System.out.println(solution3("Hello world"));
-        System.out.println(solution2("Hello world"));
-        System.out.println(solution3("Hello world"));
+        System.out.println(Arrays.toString(solution1("hello")));
+        System.out.println(Arrays.toString(solution2("hello world")));
+//        System.out.println(solution3("Hello world"));
 
     }
 
-
-    public static void solution1(String input) {
+    // assuming all chars in s string are lowercase
+    public static int[] solution1(String input) {
         int[] charCounts = new int[26];
-        for (int i : input.getBytes()) {
-            charCounts[i - 97] ++ ;
+        for (int c : input.getBytes()) {
+            charCounts[c -  97] ++ ;
+        //  charCounts[c - 'a'] ++;
         };
+        return charCounts;
     };
 
-    public static void solution2(String input) {
-        // assuming all chars in s string are lowercase
-        int[] charCounts = new int[26];
+    public static int[] solution2(String input) {
+        int[] charCounts = new int[127];
         for (char c : input.toCharArray()) {
-            charCounts[c - 'a'] ++;
+            charCounts[c] ++;
         };
         return charCounts;
     };
 
 
-    public static Map<Character, Integer> solution_3(String input) {
+    public static Map<Character, Integer> solution3(String input) {
         Map<Character, Integer> charCounts = new HashMap<>();
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
@@ -39,11 +41,5 @@ public class CountCharsInString {
         return charCounts;
     }
 
-    public static Map<Character, Integer> solution3(String input) {
-        Map<Character, Integer> charCounts = new HashMap<>();
-        for (char c : input.toCharArray()) {
-            charCounts.put(c,charCounts.getOrDefault(c,0) + 1);
-        }
-        return charCounts;
-    }
+
 }
