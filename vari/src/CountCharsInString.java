8b4745d5c3ebf5f674fccc4adeb28e8dcac47754
Boula Akladyous <akladyous@gmail.com>
2023-03-28 13:17:26 -0400

    Untracked files on algorithm

--- vari/src/CountCharsInString.java
+++ vari/src/CountCharsInString.java
@@ -3,28 +3,32 @@ import java.util.Map;
 
 public class CountCharsInString {
 
+
     public static void main(String[] args) {
         solution1("aaaA");
+        solution2("aaaA");
+        solution3("aaaA");
+
     }
 
 
-    public static void solution1(String s) {
+    public static void solution1(String input) {
         int[] charCounts = new int[26];
-        for (int i : s.getBytes()) {
+        for (int i : input.getBytes()) {
             charCounts[i - 97] ++ ;
         };
     };
 
-    public static void solution2(String s) {
+    public static void solution2(String input) {
         // assuming all chars in s string are lowercase
         int[] charCounts = new int[26];
-        for (char c : s.toCharArray()) {
+        for (char c : input.toCharArray()) {
             charCounts[c - 'a'] ++;
         };
     };
 
 
-    public static Map<Character, Integer> countCharacters(String input) {
+    public static Map<Character, Integer> solution3(String input) {
         Map<Character, Integer> charCounts = new HashMap<>();
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
