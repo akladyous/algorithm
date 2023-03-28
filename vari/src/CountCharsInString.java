04041e5450b8780b5fcd0ce2674b6c55f9411ba6
Boula Akladyous <akladyous@gmail.com>
2023-03-28 16:02:17 -0400

    Untracked files on algorithm

--- vari/src/CountCharsInString.java
+++ vari/src/CountCharsInString.java
@@ -5,9 +5,10 @@ public class CountCharsInString {
 
 
     public static void main(String[] args) {
-        solution1("aaaA");
-        solution2("aaaA");
-        solution3("aaaA");
+//        solution1("aaaA");
+        System.out.println(solution3("Hello world"));
+        System.out.println(solution2("Hello world"));
+        System.out.println(solution3("Hello world"));
 
     }
 
@@ -25,10 +26,11 @@ public class CountCharsInString {
         for (char c : input.toCharArray()) {
             charCounts[c - 'a'] ++;
         };
+        return charCounts;
     };
 
 
-    public static Map<Character, Integer> solution3(String input) {
+    public static Map<Character, Integer> solution_3(String input) {
         Map<Character, Integer> charCounts = new HashMap<>();
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
@@ -38,10 +40,10 @@ public class CountCharsInString {
     }
 
     public static Map<Character, Integer> solution3(String input) {
-        Map<Character, Integer> charsCount = new HashMap<>();
+        Map<Character, Integer> charCounts = new HashMap<>();
         for (char c : input.toCharArray()) {
-            charsCount.put(c,charsCount.getOrDefault(c,0) + 1);
-            charsCount.computeIfAbsent(c, k -> 1);
+            charCounts.put(c,charCounts.getOrDefault(c,0) + 1);
         }
+        return charCounts;
     }
 }
