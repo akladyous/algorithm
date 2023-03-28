68129738637a75a5799af2f7d1a242328ddf8739
Boula Akladyous <akladyous@gmail.com>
2023-03-28 16:36:16 -0400

    Untracked files on algorithm

--- vari/src/CountCharsInString.java
+++ vari/src/CountCharsInString.java
@@ -31,12 +31,11 @@ public class CountCharsInString {
 
     public static Map<Character, Integer> solution3(String input) {
         Map<Character, Integer> charCounts = new HashMap<>();
-        for (int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
+        for (char c : input.toCharArray()) {
             charCounts.put(c, charCounts.getOrDefault(c, 0) + 1);
-        }
+        };
         return charCounts;
-    }
+    };
 
 
 }
--- vari/src/HasDuplicateWords.java
+++ vari/src/HasDuplicateWords.java
@@ -1,8 +1,10 @@
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class HasDuplicateWords {
     public static void main(String[] args) {
+        System.out.println(" " + solution("apple pear melon apple pear apple"));
         System.out.println(" " + hasDuplicateWords_hashSet("apple pear melon apple pear apple"));
         System.out.println(" " + hasDuplicateWords_hashSet("giraffe zebra giraffe"));
         System.out.println(" " + hasDuplicateWords_hashSet("apple pear melon"));
@@ -14,9 +16,21 @@ public class HasDuplicateWords {
             if (uniqueWords.contains(word)) return true;
             uniqueWords.add(word);
         }
-        return true;
+        return false;
     };
-    public static HashMap<String, Integer> hasDuplicateWords(String string) {
+    public static boolean solution(String string) {
+        Map<String, Integer> wordsCount = new HashMap<>();
+        for (String word : string.split("\\s+")) {
+            if (wordsCount.containsKey( word)) {
+                return true;
+            } else {
+                wordsCount.put(word,wordsCount.getOrDefault(word,0 ) + 1);
+            }
+        }
+        return false;
+    }
+
+    public static HashMap<String, Integer> countDuplicateWords(String string) {
         HashMap<String, Integer> wordsCount = new HashMap<>();
         for (String word : string.split("\\s+")) {
             if (wordsCount.containsKey(word)) {
