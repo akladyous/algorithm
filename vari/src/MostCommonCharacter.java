822ce87d21ec0ea7fb553ac9014541498206a941
Boula Akladyous <akladyous@gmail.com>
2023-03-30 09:44:46 -0400

    Untracked files on algorithm

--- vari/src/MostCommonCharacter.java
+++ vari/src/MostCommonCharacter.java
@@ -1,8 +1,6 @@
 import java.util.HashMap;
 
 public class MostCommonCharacter {
-
-
     /**
      * Find the most common character in str.
      * You could use a HashMap that maps a Character key to an Int value to represent how many times a Character has
