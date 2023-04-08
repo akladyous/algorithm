825eacc8be012ff3abff5711c465f657144978ce
Boula Akladyous <akladyous@gmail.com>
2023-04-08 17:34:57 -0400

    Untracked files on algorithm

--- algoMonster/src/binarySearch/binary_search/FirstTrueInSortedBooleanArray.java
+++ algoMonster/src/binarySearch/binary_search/FirstTrueInSortedBooleanArray.java
@@ -7,7 +7,16 @@ public class FirstTrueInSortedBooleanArray {
 
     }
 
+    // [ F, F, F, T, T ,T ]
+    // size 6 - middle = (0 + 5) / 2 = 2
     public static int findBoundary(List<Boolean> arr) {
+        int left = 0;
+        int right = arr.size() - 1;
+        int boundary = -1;
+        while ( left <= right ) {
+            int middle = left + ( right - left ) / 2;
+
+        }
 
     }
 }
