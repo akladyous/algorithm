d6a2100ef4b06c6c8c002af7daea959577ca8cde
Boula Akladyous <akladyous@gmail.com>
2023-04-08 16:54:38 -0400

    Untracked files on algorithm

--- algoMonster/src/binarySearch/binary_search/VanillaBinarySearch.java
+++ algoMonster/src/binarySearch/binary_search/VanillaBinarySearch.java
@@ -1,6 +1,24 @@
 package binarySearch.binary_search;
 
+import java.util.List;
+
 public class VanillaBinarySearch {
+    public static void main(String[] args) {
+
+    }
 
 
+
+    public static int binarySearch(List<Integer> arr, int target) {
+        int left = 0;
+        int right = arr.size() - 1;
+
+        while ( left <= right ) {
+            int middle = left + (right - left) / 2; // use `(right - left) /2` to prevent `left + right` potential overflow
+
+        }
+
+
+        return -1;
+    }
 }
