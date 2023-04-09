97cad9649133cacf0124a2ad8a7435f326bd0f40
Boula Akladyous <akladyous@gmail.com>
2023-04-09 14:39:18 -0400

    Untracked files on algorithm

--- algoMonster/src/binarySearch/binary_search/FindElementInSortedArrayWithDuplicates.java
+++ algoMonster/src/binarySearch/binary_search/FindElementInSortedArrayWithDuplicates.java
@@ -1,5 +1,30 @@
 package binarySearch.binary_search;
 
-public class FindElementInSortedArrayWithDuplicates {
+import java.util.List;
 
+public class FindElementInSortedArrayWithDuplicates {
+    public static void main(String[] args) {
+
+    }
+    /*
+    arr = [1, 3, 3, 3, 3, 6, 10, 10, 10, 100]
+                       |                        1st iteration
+    */
+    public static int findFirstOccurrence(List<Integer> arr, int target) {
+        int left = 0;
+        int right = arr.size() - 1;
+        int targetIndex = -1;
+        while ( left <= right ) {
+            int middle = left + ( right - left ) / 2;
+            if ( arr.get(middle) == target ) {
+                targetIndex = middle;
+                right = middle - 1;
+            } else if (arr.get(middle) > target) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+        return targetIndex;
+    }
 }
