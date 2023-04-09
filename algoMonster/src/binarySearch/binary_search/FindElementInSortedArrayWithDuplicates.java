6d3ac690d0e6d8abfc7b11526cc18f838b72f316
Boula Akladyous <akladyous@gmail.com>
2023-04-09 15:22:46 -0400

    Untracked files on algorithm

--- algoMonster/src/binarySearch/binary_search/FindElementInSortedArrayWithDuplicates.java
+++ algoMonster/src/binarySearch/binary_search/FindElementInSortedArrayWithDuplicates.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 public class FindElementInSortedArrayWithDuplicates {
     public static void main(String[] args) {
-        System.out.println(findFirstOccurrence(List.of(1, 3, 3, 3, 3, 4, 6, 10, 10, 10, 100), 6));
+        System.out.println(findFirstOccurrence(List.of(1, 2, 2, 3, 3, 4, 4, 5, 6, 10, 10, 10, 100), 10));
+        //6, 10, 10, 10, 100
     }
+
     /*
     arr = [1, 3, 3, 3, 3, 6, 10, 10, 10, 100]
                        |                        1st iteration®´
@@ -13,18 +15,16 @@ public class FindElementInSortedArrayWithDuplicates {
     public static int findFirstOccurrence(List<Integer> arr, int target) {
         int left = 0;
         int right = arr.size() - 1;
-        int targetIndex = -1;
+        int boundaryIndex = - 1;
         while ( left <= right ) {
-            int middle = left + ( right - left ) / 2;
-            if ( arr.get(middle) == target ) {
-                targetIndex = middle;
-                right = middle - 1;
-            } else if (arr.get(middle) > target) {
-                right = middle - 1;
+            int mid = left + (right - left) / 2;
+            if (arr.get(mid) >= target) {
+                boundaryIndex = mid;
+                right = mid - 1;
             } else {
-                left = middle + 1;
+                left = mid + 1;
             }
         }
-        return targetIndex;
+        return boundaryIndex;
     }
-}
+};
