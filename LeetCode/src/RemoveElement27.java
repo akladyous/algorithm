5f24a374e279017964de363cf549facc019588af
Boula Akladyous <akladyous@gmail.com>
2023-04-11 11:03:23 -0400

    Untracked files on algorithm

--- LeetCode/src/RemoveElement27.java
+++ LeetCode/src/RemoveElement27.java
@@ -2,4 +2,13 @@ public class RemoveElement27 {
     public static void main(String[] args) {
 
     }
+
+    public static int removeElement(int[] nums, int val) {
+        if (nums.length == 0) return 0;
+
+        int left = 0;
+        for ( int right = 0; right < nums.length; right++ ) {
+
+        }
+    }
 }
