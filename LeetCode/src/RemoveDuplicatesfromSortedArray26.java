ca8f86294a8db105aa2fec3e8396288398dc63e5
Boula Akladyous <akladyous@gmail.com>
2023-04-11 10:29:21 -0400

    Untracked files on algorithm

--- LeetCode/src/RemoveDuplicatesfromSortedArray26.java
+++ LeetCode/src/RemoveDuplicatesfromSortedArray26.java
@@ -2,4 +2,16 @@ public class RemoveDuplicatesfromSortedArray26 {
     public static void main(String[] args) {
 
     }
+
+    public static int removeDuplicates(int[] nums) {
+        int left = 0;
+        for (int right = 0; right < nums.length; right ++) {
+            if ( nums[right] > nums[left]) {
+                left ++;
+                nums[left] = nums[right];
+            }
+        }
+        return left + 1;
+    }
 }
+
