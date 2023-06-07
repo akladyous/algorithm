3846fb5be687db664540a21b437044fd866ac5f8
Boula Akladyous <akladyous@gmail.com>
2023-06-07 17:33:58 -0400

    Untracked files on algorithm

--- LeetCode/src/easy/twoSum/TwoSumSolution.java
+++ LeetCode/src/easy/twoSum/TwoSumSolution.java
@@ -11,7 +11,7 @@ public class TwoSumSolution {
         result = twoSum(new int[]{2,7,11,15}, 9);
         System.out.println(Arrays.toString(result));
 
-        result = twoSum(new int[]{3,3}, 6);
+        result = twoSum(new int[]{3,1,5,6,4,3}, 6);
         System.out.println(Arrays.toString(result));
 
         result = twoSum(new int[]{3,2,4}, 6);
@@ -19,8 +19,9 @@ public class TwoSumSolution {
 
     }
 
-    public static int[] twoSum(int[] nums, int target) {
+    //
 
+    public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> indices = new HashMap<>();
         for (int i=0; i < nums.length; i++) {
             int diff = target - nums[i];
