b4b32a4627d817983cb780a58c613d778b0c55a2
Boula Akladyous <akladyous@gmail.com>
2023-06-08 09:16:52 -0400

    Untracked files on algorithm

---
+++ LeetCode/src/easy/thirteen_roman_to_integer/RomanToInteger_solution_2.java
@@ -0,0 +1,41 @@
+package easy.thirteen_roman_to_integer;
+import java.util.HashMap;
+
+public class RomanToInteger_solution_2 {
+    public static void main(String[] args) {
+        String romanNumeral = "XXIV";
+        int number = romanToInteger(romanNumeral);
+        System.out.println(number);
+    }
+
+    public static int romanToInteger(String romanNumeral) {
+        HashMap<Character, Integer> romanValues = new HashMap<>();
+        romanValues.put('I', 1);
+        romanValues.put('V', 5);
+        romanValues.put('X', 10);
+        romanValues.put('L', 50);
+        romanValues.put('C', 100);
+        romanValues.put('D', 500);
+        romanValues.put('M', 1000);
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = romanNumeral.length() - 1; i >= 0; i--) {
+            char currentChar = romanNumeral.charAt(i);
+            int currentValue = romanValues.get(currentChar);
+
+            if (currentValue >= prevValue) {
+                result += currentValue;
+            } else {
+                result -= currentValue;
+            }
+
+            prevValue = currentValue;
+        }
+
+        return result;
+    }
+
+
+}
