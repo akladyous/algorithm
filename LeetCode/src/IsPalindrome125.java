b50957e63e41dceb6cce351fcefe73a3779e5285
Boula Akladyous <akladyous@gmail.com>
2023-06-08 17:27:01 -0400

    Untracked files on algorithm

--- LeetCode/src/IsPalindrome125.java
+++ LeetCode/src/IsPalindrome125.java
@@ -1,24 +1,37 @@
 public class IsPalindrome125 {
 
     public static void main(String[] args) {
-        System.out.println(solution("A man, a plan, a canal: Panama"));
+        String s1 = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s1)); // true
+
+        String s2 = "race a car";
+        System.out.println(isPalindrome(s2)); // false
+
+        String s3 = " ";
+        System.out.println(isPalindrome(s3)); // true
     };
 
-    public static Boolean solution(String s) {
-        int left = 0, right = s.length() - 1;
+    public static Boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
         while (left < right) {
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-                left ++;
-            };
-            while (right > left && !Character.isLetterOrDigit(s.charAt(right))) {
-                right --;
-            };
-            if (Character.toLowerCase(s.charAt(right)) != Character.toLowerCase(s.charAt(left))) {
-                return false;
+            char leftChar = s.charAt(left);
+            char rightChar = s.charAt(right);
+
+            if (!Character.isLetterOrDigit(leftChar)) {
+                left++;
+            } else if (!Character.isLetterOrDigit(rightChar)) {
+                right--;
+            } else {
+                if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
+                    return false;
+                }
+                left++;
+                right--;
             }
-            left ++;
-            right --;
         }
+
         return true;
     }
 }
