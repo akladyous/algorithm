80cdafefe47a2c023d5036be31fed4aa30360a72
Boula Akladyous <akladyous@gmail.com>
2023-06-08 10:11:22 -0400

    Untracked files on algorithm

--- LeetCode/src/easy/longest_common_prefix_14/LongestCommonPrefix_solution.java
+++ LeetCode/src/easy/longest_common_prefix_14/LongestCommonPrefix_solution.java
@@ -5,16 +5,21 @@ public class LongestCommonPrefix_solution {
     public static void main(String[] args) {
         String[] strs1 = {"flower", "flow", "flight"};
         String longestPrefix1 = longestCommonPrefix(strs1);
-        System.out.println(longestPrefix1); // Output: "fl"
+        System.out.println(longestPrefix1);
 
         String[] strs2 = {"dog", "racecar", "car"};
         String longestPrefix2 = longestCommonPrefix(strs2);
-        System.out.println(longestPrefix2); // Output: ""
+        System.out.println(longestPrefix2);
     }
 
     public static String longestCommonPrefix(String[] strs) {
 
 
+        String prefix = "";
+        for (char c : strs[0].toCharArray()) {
+            
+        }
 
+        return null;
     }
 }
