e84cc11bb6a044697f33b310b789c82538746a06
Boula Akladyous <akladyous@gmail.com>
2023-06-10 01:09:23 -0400

    Untracked files on algorithm

---
+++ LeetCode/src/easy/FizzBuzz_412.java
@@ -0,0 +1,30 @@
+package easy;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz_412 {
+    public static void main(String[] args) {
+        int n = 9;
+        List<String> fizzBuzzList = fizzBuzz(n);
+        System.out.println(fizzBuzzList);
+
+    }
+    public static List<String> fizzBuzz(int n) {
+        List<String> output = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                output.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                output.add("Fizz");
+            } else if (i % 5 == 0) {
+                output.add("Buzz");
+            } else {
+                output.add(Integer.toString(i));
+            }
+        }
+        return output;
+    }
+
+
+}
