5fbbd04d4c42e281d64c01af4452d37bba676903
Boula Akladyous <akladyous@gmail.com>
2023-06-10 14:21:07 -0400

    Untracked files on algorithm

--- LeetCode/src/easy/Practice.java
+++ LeetCode/src/easy/Practice.java
@@ -18,17 +18,23 @@ public class Practice {
     public static int maxProfit(int[] prices) {
         int maxProfit = 0;
         int left = 0;
-        int right = 0;
+        int right = 1;
         /*
             [2,1,2,1,0,1,2]
              L<R -> false       -> increment Left and Right pointers
             [2,1,2,1,0,1,2]
                L<R -> true      -> update maxProfit with [right - left] 2-1
                                 -> increment Right pointer
-
         */
-
-
+        while ( right < prices.length ) {
+            if ( prices[left] < prices[right] ) {
+                int profit = prices[right] - prices[left];
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                left ++;
+            }
+            right ++;
+        }
         return maxProfit;
     }
 }
