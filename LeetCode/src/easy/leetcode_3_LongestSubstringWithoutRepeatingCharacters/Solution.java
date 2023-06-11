44ab5b029d8fecf584c13bab86e93216da52d3a2
Boula Akladyous <akladyous@gmail.com>
2023-06-10 22:17:06 -0400

    Untracked files on algorithm

---
+++ LeetCode/src/easy/leetcode_3_LongestSubstringWithoutRepeatingCharacters/Solution.java
@@ -0,0 +1,40 @@
+package easy.leetcode_3_LongestSubstringWithoutRepeatingCharacters;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String testCase = "";
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring("bbbbb"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+    }
+    /*
+        ↓                       window
+        P  W  W  K  E  W        L=0 R=0 [P] -> L++
+        ↑  ↑                    L=0 R=0 [P, W] -> L++
+        ↑     ↑
+
+    */
+
+    static int lengthOfLongestSubstring(String s) {
+        int left = 0;
+        int right = 0;
+        int max = 0;
+        Set<Character> window = new HashSet<>();
+        while ( right < s.length() ) {
+            if ( !window.contains(s.charAt(right))) {
+                window.add(s.charAt(right));
+                right ++;
+                max = Math.max(max,window.size());
+            } else {
+                window.remove(s.charAt(left));
+                left ++;
+            }
+        }
+        return max;
+    }
+}
