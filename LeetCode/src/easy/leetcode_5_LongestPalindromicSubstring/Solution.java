5b90239ed59d1a4a2867cec2f5d1c8bbf3d161e7
Boula Akladyous <akladyous@gmail.com>
2023-06-12 18:27:34 -0400

    Untracked files on algorithm

--- LeetCode/src/easy/leetcode_5_LongestPalindromicSubstring/Solution.java
+++ LeetCode/src/easy/leetcode_5_LongestPalindromicSubstring/Solution.java
@@ -16,23 +16,36 @@ public class Solution {
         if (s == null || s.length() < 1) {
             return "";
         }
-
+        // star & end to keep track of matching palindrome
         int start = 0;
         int end = 0;
 
         for (int i = 0; i < s.length(); i++) {
             int len1 = expandAroundCenter(s, i, i);
             int len2 = expandAroundCenter(s, i, i + 1);
-            int len = Math.max(len1, len2);
 
+            // represents the length of the longest palindromic substring found so far.
+            int len = Math.max(len1, len2);
+            // if current palindrome length is greater than the length of the last palindrome
             if (len > end - start) {
-                start = i - (len - 1) / 2;
+                start = i - ( (len - 1) / 2 );
+                /*
+                (len - 1) gives us the number of characters between the center of the palindrome and its edge on one side.
+                (len - 1) / 2 calculates the number of characters to move towards the left from the center to reach the start index of the palindrome.
+                 */
                 end = i + len / 2;
             }
         }
 
         return s.substring(start, end + 1);
     }
+/*
+    X X X B A B A D       first match bab
+      I index = 4
+      start = i - len - 1 -> 4 - (1-1)
+
+*/
+
 
     static int expandAroundCenter(String s, int left, int right) {
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
@@ -42,4 +55,5 @@ public class Solution {
 
         return right - left - 1;
     }
+
 }
