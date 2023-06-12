c3e7895131bc2ebdbb340a78c88dd1264118234f
Boula Akladyous <akladyous@gmail.com>
2023-06-12 14:30:14 -0400

    Untracked files on algorithm

--- vari/src/IsPalindrom.java
+++ vari/src/IsPalindrom.java
@@ -7,37 +7,50 @@ public class IsPalindrom {
         System.out.println(solution2("abcba"));
         System.out.println(solution2("abcde"));
         System.out.println(solution2("123454321"));
-
     }
 
-    public static Boolean solution1(String str) {
-        int left = str.length() /2, right = left;
-        if (str.length() %2 == 0) left--;
+    public static Boolean solution1(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while ( left < right ) {
+            if ( s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left ++;
+            right --;
+        }
+        return true;
+    };
+
+    public static Boolean solution2(String s) {
+        int left = s.length() /2, right = left;
+        if (s.length() %2 == 0) left--;
         String palindrome = "";
         while (
                 left >= 0 &&
                 left <= right &&
-                right < str.length() &&
-                str.charAt(left) == str.charAt(right)
+                right < s.length() &&
+                s.charAt(left) == s.charAt(right)
         ) {
             if ( right - left >= 1) {
-                palindrome = str.substring(left, right + 1);
+                palindrome = s.substring(left, right + 1);
             }
             left --;
             right ++;
         }
-        return palindrome == str;
+        return palindrome == s;
     }
 
-    public static Boolean solution2(String str) {
-        int left = str.length() /2, right = left;
-        if (str.length() %2 == 0) left--;
+
+    public static Boolean solution3(String s) {
+        int left = s.length() /2, right = left;
+        if (s.length() %2 == 0) left--;
         while (
                 left >= 0 &&
                 left <= right &&
-                right < str.length()
+                right < s.length()
         ) {
-            if ( str.charAt(left) != str.charAt(right) ) return false;
+            if ( s.charAt(left) != s.charAt(right) ) return false;
             left --;
             right ++;
         }
