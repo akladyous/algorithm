005c4cb2b14d7ab2263d69b0979b057740ca7a10
Boula Akladyous <akladyous@gmail.com>
2023-06-13 12:13:36 -0400

    Untracked files on algorithm

--- LeetCode/src/easy/Practice.java
+++ LeetCode/src/easy/Practice.java
@@ -6,10 +6,22 @@ import java.util.Map;
 
 public class Practice {
   public static void main(String[] args) {
-
+    System.out.println(isPalindrome_1(121));
+    System.out.println(isPalindrome_1(1410110141));
   }
 
-
+  public boolean isPalindrome_1(int x) {
+    int xCopy = x;
+    int xReversed = 0;
+    while (x > 0) {
+      // 121
+      // move the right digit in xReversed by one digit by multiplying xReversed by 10
+      int lestDigit = x & 10; // get the right digit
+      xReversed = xReversed * 10 + lestDigit;
+      x /= 10;
+    }
+    return xReversed == x;
+  }
 
 
 
