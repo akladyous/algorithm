dab793bfb96dca1c2e7e174bd3a48c866f9e3852
Boula Akladyous <akladyous@gmail.com>
2023-06-13 12:25:39 -0400

    Untracked files on algorithm

--- LeetCode/src/easy/Practice.java
+++ LeetCode/src/easy/Practice.java
@@ -7,23 +7,30 @@ import java.util.Map;
 public class Practice {
   public static void main(String[] args) {
     System.out.println(isPalindrome_1(121));
-    System.out.println(isPalindrome_1(1410110141));
-    System.out.println(isPalindrome_1(121));
-    System.out.println(isPalindrome_1(1410110141));
+    System.out.println(isPalindrome_1(1221));
+    System.out.println(isPalindrome_1(1234));
   }
 
   static boolean isPalindrome_1(int x) {
-    int xCopy = x;
-    int xReversed = 0;
-    while (x > 0) {
-      int lsg = x & 10;
-      xReversed = xReversed * 10 + lsg;
+    int reversedNumber = 0;
+    int originalNumber = x;
+    while ( x > 0 ) {
+      int leastSignificantDigit = x % 10;
+      reversedNumber = reversedNumber * 10 + leastSignificantDigit;
       x /= 10;
     }
-    return xReversed == x;
+    return originalNumber == reversedNumber;
   }
 
 
+  static boolean isPalindrome_2(int x) {
+    String number = Integer.toString(x);
+    int left = 0;
+    int right = number.length() - 1;
+    while ( left < right ) {
+
+    }
+  }
 
 }
 
