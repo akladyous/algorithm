328d99cd7169418a389ad15aac0699af36b36fb7
Boula Akladyous <akladyous@gmail.com>
2023-06-13 13:47:35 -0400

    Untracked files on algorithm

--- LeetCode/src/easy/Practice.java
+++ LeetCode/src/easy/Practice.java
@@ -1,6 +1,7 @@
 package easy;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,14 +16,18 @@ public class Practice {
     System.out.println("( " + longestPrefix2 + " )");
   }
 
-
   public static String longestCommonPrefix(String[] strs) {
     String prefix = strs[0];
 
     for (int i=1; i<strs.length; i++) {
-
+      while ( !strs[i].startsWith(prefix) ) {
+        prefix = prefix.substring(0, prefix.length() - 1);
+        if (prefix.isEmpty()) {
+          return "";
+        };
+      }
     }
-
+    return prefix;
   }
 
 
