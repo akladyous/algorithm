d3d540bb4cef959f6cd3da970b557130d5e507aa
Boula Akladyous <akladyous@gmail.com>
2023-06-14 15:07:19 -0400

    Untracked files on algorithm

--- LeetCode/src/easy/Practice.java
+++ LeetCode/src/easy/Practice.java
@@ -4,21 +4,23 @@ import java.util.*;
 
 public class Practice {
   public static void main(String[] args) {
-    System.out.println(isIsomorphic2("bbbaaaba", "aaabbbba"));
-    System.out.println(isIsomorphic("egg", "acc"));
+    System.out.println(isIsomorphic1("bbbaaaba", "aaabbbba"));
+    System.out.println(isIsomorphic1("egg", "acc"));
   }
 
-  static  boolean isIsomorphic(String s, String t) {
-    int[] sCounts = new int[128];
-    int[] tCounts = new int[128];
+  static  boolean isIsomorphic1(String s, String t) {
+    HashMap<Character, Character> mapS = new HashMap<>();
+    HashMap<Character, Character> mapT = new HashMap<>();
 
-    for (int i=0; i < s.length(); i++) {
-      sCounts[ s.charAt(i) ] ++;
-      tCounts[ t.charAt(i) ] ++;
+    for (int i=0; i< s.length(); i++) {
+      char charS = s.charAt(i);
+      char charT = t.charAt(i);
 
-      if(sCounts[s.charAt(i)] != tCounts[t.charAt(i)]) {
+      if ( mapS.containsKey(charS) && mapS.get(charS) != charT || mapT.containsKey(charT) && mapT.get(charT) != charS ) {
         return false;
       }
+      mapS.put(charS, charT);
+      mapT.put(charT, charS);
     }
     return true;
   }
@@ -27,23 +29,12 @@ public class Practice {
       int[] tCounts = new int[128];
 
       for(int i = 0; i < s.length(); i++){
-        char sChar = s.charAt(i);
-        char tChar = t.charAt(i);
 
-        int sCountBefore = sCounts[s.charAt(i)];
-        int tCountBefore = tCounts[t.charAt(i)];
-
-        if(sCounts[(int) (s.charAt(i) - 97) ] != tCounts[ (int) (t.charAt(i) - 97) ])
+        if(sCounts[(int) (s.charAt(i) - 97) ] != tCounts[ (int) (t.charAt(i) - 97) ]) {
           return false;
-        // [_1] [_2] [_3] [43] [53] [63] [67]
-        // [1_] [2_] [3_] [34] [35] [36] [
+        }
         sCounts[ (int) (s.charAt(i) - 97) ] = i + 1;
         tCounts[ (int) (t.charAt(i) - 97) ] = i + 1;
-
-
-        int sCountAfter = sCounts[s.charAt(i)];
-        int tCountAfter = tCounts[t.charAt(i)];
-        System.out.println("-");
       }
       return true;
   }
