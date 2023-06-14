7604206e79cd910250afbf08f07a946a2f44ea60
Boula Akladyous <akladyous@gmail.com>
2023-06-14 10:04:02 -0400

    Untracked files on algorithm

--- LeetCode/src/easy/Practice.java
+++ LeetCode/src/easy/Practice.java
@@ -4,28 +4,15 @@ import java.util.*;
 
 public class Practice {
   public static void main(String[] args) {
-    int[] nums1 = {1, 1, 2};
-    int k1 = removeDuplicatesFromSortedArray_26(nums1);
-    System.out.println(k1); // Output: 2
-
-    int[] nums2 = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-    int k2 = removeDuplicatesFromSortedArray_26(nums2);
-    System.out.println(k2); // Output: 5
-
-    int[] nums3 = {1, 2, 3, 3, 4, 4, 5};
-    int k3 = removeDuplicatesFromSortedArray_26(nums3);
-    System.out.println(k3); // Output: 5
-
-
+    System.out.println(Arrays.toString(countCharsInString1("aabbcc")));
   }
 
-  public static int removeDuplicatesFromSortedArray_26(int[] nums) {
-    int left = 0;
-    for (int right=0; right < nums.length; right ++) {
-
+  static int[] 1(String s) {
+    int[] counts = new int[26];
+    for (int n: s.getBytes()) {
+      counts[n - 97] ++;
     }
-
-  };
-
+    return counts;
+  }
 
 }
