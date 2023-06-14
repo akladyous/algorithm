2922c7a85a305fb004e3cb3147a839600da08ff3
Boula Akladyous <akladyous@gmail.com>
2023-06-14 16:07:44 -0400

    Untracked files on algorithm

--- LeetCode/src/easy/leetcode_205_IsomorphicStrings/Solution.java
+++ LeetCode/src/easy/leetcode_205_IsomorphicStrings/Solution.java
@@ -3,6 +3,12 @@ package easy.leetcode_205_IsomorphicStrings;
 import java.util.HashMap;
 
 public class Solution {
+  public static void main(String[] args) {
+    System.out.println(isIsomorphic("bbbaaaba", "aaabbbba"));
+    System.out.println(isIsomorphic("egg", "acc"));
+    HashMap<Integer, Integer> nums = new HashMap<>();
+
+  }
 
   static boolean isIsomorphic(String s, String t) {
     HashMap<Character, Character> mapS = new HashMap<>();
