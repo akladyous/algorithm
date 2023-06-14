f0f5e33b51adf36ff3011d0ac5c055438a3ee35b
Boula Akladyous <akladyous@gmail.com>
2023-06-14 16:18:57 -0400

    Untracked files on algorithm

--- LeetCode/src/easy/leetcode_205_IsomorphicStrings/Solution.java
+++ LeetCode/src/easy/leetcode_205_IsomorphicStrings/Solution.java
@@ -5,12 +5,31 @@ import java.util.HashMap;
 public class Solution {
   public static void main(String[] args) {
     System.out.println(isIsomorphic("bbbaaaba", "aaabbbba"));
-    System.out.println(isIsomorphic("egg", "acc"));
+    System.out.println(isIsomorphic1("egg", "acc"));
     HashMap<Integer, Integer> nums = new HashMap<>();
 
   }
 
   static boolean isIsomorphic(String s, String t) {
+    int[] mapS = new int[128];
+    int[] mapT = new int[128];
+
+    for (int i = 0; i < s.length(); i++) {
+      char charS = s.charAt(i);
+      char charT = t.charAt(i);
+
+      if (mapS[charS] != 0 && mapS[charS] != charT || mapT[charT] != 0 && mapT[charT] != charS) {
+        return false;
+      }
+
+      mapS[charS] = charT;
+      mapT[charT] = charS;
+    }
+
+    return true;
+  }
+
+  static boolean isIsomorphic1(String s, String t) {
     HashMap<Character, Character> mapS = new HashMap<>();
     HashMap<Character, Character> mapT = new HashMap<>();
 
@@ -23,6 +42,23 @@ public class Solution {
       }
       mapS.put(charS, charT);
       mapT.put(charT, charS);
+    }
+    return true;
+  }
+
+
+
+  static  boolean isIsomorphic3(String s, String t) {
+    int[] sCounts = new int[128];
+    int[] tCounts = new int[128];
+
+    for(int i = 0; i < s.length(); i++){
+
+      if(sCounts[(int) (s.charAt(i) - 97) ] != tCounts[ (int) (t.charAt(i) - 97) ]) {
+        return false;
+      }
+      sCounts[ (int) (s.charAt(i) - 97) ] = i + 1;
+      tCounts[ (int) (t.charAt(i) - 97) ] = i + 1;
     }
     return true;
   }
