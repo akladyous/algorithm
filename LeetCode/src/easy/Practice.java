691ed7c59a0c9ca739e88db113fbcff3869fb374
Boula Akladyous <akladyous@gmail.com>
2023-06-14 10:08:32 -0400

    Untracked files on algorithm

--- LeetCode/src/easy/Practice.java
+++ LeetCode/src/easy/Practice.java
@@ -4,10 +4,10 @@ import java.util.*;
 
 public class Practice {
   public static void main(String[] args) {
-    System.out.println(Arrays.toString(countCharsInString1("aabbcc")));
+    System.out.println(Arrays.toString(countCharsInString2("aabbbcccc")));
   }
 
-  static int[] 1(String s) {
+  static int[] countCharsInString1(String s) {
     int[] counts = new int[26];
     for (int n: s.getBytes()) {
       counts[n - 97] ++;
@@ -15,4 +15,11 @@ public class Practice {
     return counts;
   }
 
+  static int[] countCharsInString2(String s) {
+    int[] counts = new int[127];
+    for (char c: s.toCharArray()) {
+      counts[c] ++;
+    }
+    return counts;
+  }
 }
