471d0614ab5c079fd6177f1c6576164502d83b66
Boula Akladyous <akladyous@gmail.com>
2023-06-15 12:03:31 -0400

    Untracked files on algorithm

--- LeetCode/src/easy/leetcode_392_IsSubsequence/Solution.java
+++ LeetCode/src/easy/leetcode_392_IsSubsequence/Solution.java
@@ -6,20 +6,31 @@ import java.util.Set;
 public class Solution {
 
     public static void main(String[] args) {
-        String s = "ahbgdc";
-        String t = "abc";
+        String s = "abc";
+        String t = "ahbgdc";
         System.out.println(isSubsequence(s, t));
     }
-
     static boolean isSubsequence(String s, String t) {
         int sPointer = 0;
         int tPointer = 0;
-        while (sPointer < s.length() && tPointer < t.length()) {
-            if (s.charAt(sPointer) && t.charAt(tPointer)) {
 
+        while (sPointer < s.length() && tPointer < t.length()) {
+            if (s.charAt(sPointer) == t.charAt(tPointer)) {
+                sPointer++;
             }
+            tPointer++;
         }
         return sPointer == s.length();
     }
 
+    static boolean solution2(String s, String t) {
+        int sPointer = 0;
+
+        for (int tPointer = 0; tPointer < t.length(); tPointer++) {
+            if (sPointer < s.length() && s.charAt(sPointer) == t.charAt(tPointer)) {
+                sPointer++;
+            }
+        }
+        return sPointer == s.length();
+    }
 }
