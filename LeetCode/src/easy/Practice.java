8ae8a35687cb32ad2074dbb2f7d951d1c57c2729
Boula Akladyous <akladyous@gmail.com>
2023-06-15 00:16:14 -0400

    Untracked files on algorithm

--- LeetCode/src/easy/Practice.java
+++ LeetCode/src/easy/Practice.java
@@ -4,8 +4,18 @@ import java.util.*;
 
 public class Practice {
   public static void main(String[] args) {
-
+    System.out.println(containsDuplicate(new int[] {1,2,3,1}));
   }
 
+  static boolean containsDuplicate(int[] nums) {
+    HashSet<Integer> counts = new HashSet<>();
+    for (int num: nums) {
+      if (counts.contains(num)) {
+        return true;
+      }
+      counts.add(num);
+    }
+    return false;
+  }
 }
 
