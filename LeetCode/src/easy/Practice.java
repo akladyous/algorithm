34c8ec09f08a85fefdaf4218b43d627b20db8d89
Boula Akladyous <akladyous@gmail.com>
2023-06-15 17:13:41 -0400

    Untracked files on algorithm

--- LeetCode/src/easy/Practice.java
+++ LeetCode/src/easy/Practice.java
@@ -4,34 +4,5 @@ import java.util.*;
 
 public class Practice {
 
-  public static void main(String[] args) {
-      System.out.println(generate(5));
-  }
-
-    static List<List<Integer>> generate(int numRows) {
-      List<List<Integer>> pascal = new ArrayList<>();
-      for (int i=0; i< numRows; i++) {
-          List<Integer> row = new ArrayList<>();
-          for (int j=1; j<= i+1 ; j++) {
-              if (j == 1 ) {
-                  row.add(1);
-              } else {
-
-              }
-          }
-          pascal.add(row);
-      }
-      return pascal;
-    }
-
 
 }
-/*
-[
-    [1]                 [0, 0, 1, 0, 0]
-    [0, 0]              [0, 1, 0, 1, 0]
-    [0, 0, 0]           [0, 0, 0, 0, 0]
-    [0, 0, 0, 0]        [0, 0, 0, 0, 0]
-    [0, 0, 0, 0, 0]     [1, 4, 6, 4, 1]
-]
-*/
--- LeetCode/src/easy/leetcode_27_RemoveElement/Solution.java
+++ LeetCode/src/easy/leetcode_27_RemoveElement/Solution.java
@@ -3,11 +3,23 @@ package easy.leetcode_27_RemoveElement;
 public class Solution {
 
   public static void main(String[] args) {
-
+    System.out.println(removeElement(new int[]{3,2,2,3}, 3));
+    System.out.println(removeElement(new int[]{0,1,2,2,3,0,4,2}, 2));
   }
-
+  /*
+  In this solution, we use two pointers: `right` to iterate over the elements of the nums array,
+  and `left` to keep track of the position where the non-val elements should be placed.
+  If nums[right] is not equal to `val`, we move it to the front of the array at position `left` and increment `left`.
+  Finally, we return `left`, which represents the number of elements not equal to val.
+  */
   static int removeElement(int[] nums, int val) {
-
-    return 0;
+    int left = 0;
+    for (int right=0; right < nums.length; right ++) {
+      if (nums[right] != val) {
+        nums[left] = nums[right];
+        left ++;
+      }
+    }
+    return left;
   }
 }
