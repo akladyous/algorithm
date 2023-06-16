300450095766c034fd7eb99b821d4ce9b7aeac3e
Boula Akladyous <akladyous@gmail.com>
2023-06-16 14:38:24 -0400

    Untracked files on algorithm

--- LeetCode/src/easy/Practice.java
+++ LeetCode/src/easy/Practice.java
@@ -6,11 +6,25 @@ public class Practice {
     public static void main(String[] args) {
         String s = "abc";
         String t = "ahbgdc";
-        System.out.println(isSubsequence(s, t));
+        System.out.println(isSubsequence(s, t));  // Output: true
+
+        String s1 = "abc";
+        String t1 = "ahbgdc";
+        System.out.println(isSubsequence(s1, t1)); // Output: true
+
+        // Test case 2: s = "axc", t = "ahbgdc"
+        String s2 = "axc";
+        String t2 = "ahbgdc";
+        System.out.println(isSubsequence(s2, t2)); // Output: false
     }
 
-    static boolean isSubsequence(String s, String t) {
-
-
+    static boolean isSubsequence(String s, String t) { // [s] -> is a subsequence of [t]
+        int sPointer = 0;
+        for (int tPointer=0; tPointer < t.length(); tPointer ++) {
+            if (sPointer < s.length() && t.charAt(tPointer) == s.charAt(sPointer)) {
+                sPointer ++;
+            }
+        }
+        return sPointer == s.length();
     }
 }
