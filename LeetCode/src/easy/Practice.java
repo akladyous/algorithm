432a235210ad4177b83cbbab2ead6192de970484
Boula Akladyous <akladyous@gmail.com>
2023-06-16 14:27:05 -0400

    Untracked files on algorithm

--- LeetCode/src/easy/Practice.java
+++ LeetCode/src/easy/Practice.java
@@ -4,8 +4,13 @@ import java.util.*;
 
 public class Practice {
     public static void main(String[] args) {
-
+        String s = "abc";
+        String t = "ahbgdc";
+        System.out.println(isSubsequence(s, t));
     }
 
+    static boolean isSubsequence(String s, String t) {
 
+
+    }
 }
--- LeetCode/src/easy/leetcode_392_IsSubsequence/Solution.java
+++ LeetCode/src/easy/leetcode_392_IsSubsequence/Solution.java
@@ -3,6 +3,23 @@ package easy.leetcode_392_IsSubsequence;
 import java.util.HashSet;
 import java.util.Set;
 
+/*
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting
+some (can be none) of the characters without disturbing the relative positions
+of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+
+
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
+*/
 public class Solution {
 
     public static void main(String[] args) {
@@ -10,6 +27,7 @@ public class Solution {
         String t = "ahbgdc";
         System.out.println(isSubsequence(s, t));
     }
+
     static boolean isSubsequence(String s, String t) {
         int sPointer = 0;
         int tPointer = 0;
