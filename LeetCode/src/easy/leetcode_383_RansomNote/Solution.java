51bfad6c529a671ec73d12fa8a8c8a90308e0542
Boula Akladyous <akladyous@gmail.com>
2023-06-16 09:36:08 -0400

    Untracked files on algorithm

--- LeetCode/src/easy/leetcode_383_RansomNote/Solution.java
+++ LeetCode/src/easy/leetcode_383_RansomNote/Solution.java
@@ -2,11 +2,12 @@ package easy.leetcode_383_RansomNote;
 
 public class Solution {
     public static void main(String[] args) {
-
+        System.out.println(canConstruct("a", "b")); // false
     }
 
     static boolean canConstruct(String ransomNote, String magazine) {
-      for (int r=0; r < ransomNote.length(); r++) {
+      for (int i=0; i < ransomNote.length(); i++) {
+        char r = ransomNote.charAt(i);
 
       }
 
