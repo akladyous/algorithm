da84b9b212f98412c6240d8e81eb337c604b01fa
Boula Akladyous <akladyous@gmail.com>
2023-06-16 10:00:11 -0400

    Untracked files on algorithm

--- LeetCode/src/easy/leetcode_383_RansomNote/Solution.java
+++ LeetCode/src/easy/leetcode_383_RansomNote/Solution.java
@@ -8,12 +8,41 @@ public class Solution {
     }
 
     static boolean canConstruct(String ransomNote, String magazine) {
+        int[] magazineFreq = new int[26];
+
+        for (int i=0; i<magazine.length(); i++) {
+            int c = magazine.charAt(i) - 97;
+            magazineFreq[ c ] ++;
+        }
+
+        for (int i=0; i< ransomNote.length(); i++) {
+            int c = ransomNote.charAt(i) - 97;
+            if ( magazineFreq[c] == 0 ) {
+                return false;
+            } else {
+                magazineFreq[c] --;
+            }
+        }
+        return true;
+    }
+
+    static boolean canConstruct2(String ransomNote, String magazine) {
       for (int i=0; i < ransomNote.length(); i++) {
         char r = ransomNote.charAt(i);
 
+        int matchIndex = -1;
+        for (int j=0; j < magazine.length(); j++) {
+            if (magazine.charAt(j) == ransomNote.charAt(i)) {
+                matchIndex = j;
+            }
+        }
+        if (matchIndex == -1) {
+            return false;
+        }
+        magazine = magazine.substring(0, matchIndex) + magazine.substring(matchIndex + 1);
       }
-
-
       return true;
     }
+
+
 }
