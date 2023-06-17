2db4045927a02a37f6bd66746a4624c38dbdac93
Boula Akladyous <akladyous@gmail.com>
2023-06-17 15:37:33 -0400

    Untracked files on algorithm

--- LeetCode/algorithms/easy/leetcode_643_MaximumAverageSubarrayI/Solution.java
+++ LeetCode/algorithms/easy/leetcode_643_MaximumAverageSubarrayI/Solution.java
@@ -1,27 +1,29 @@
 package easy.leetcode_643_MaximumAverageSubarrayI;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(findMaxAverage(new int[]{1,12,-5,-6,50,3}, 4));
+        System.out.println(findMaxAverage(new int[]{1, 12, -5, -6, 50, 3}, 4));
     }
 
     static double findMaxAverage(int[] nums, int k) {
-        // 1,12,-5,-6,50,3
-        // 1 2 3 4 5 6
-        int left = 0;
-        int right = nums.length - 1;
-        int currentSum = 0;
-        double max = 0;
-        for (int i=0; i < k; i++) {
-            for (int j=i; j<k && j<nums.length; j++) {
-             currentSum += nums[j];
+        List<Integer> arr = new ArrayList<>();
+        double maxAverage = Double.NEGATIVE_INFINITY;
+        for (int i = 0; i <= nums.length - k; i++) {
+            int sum = 0;
+            arr.clear();
+            for (int j = i; j < i+k; j++) {
+                sum += nums[j];
+                arr.add(nums[j]);
             }
-            double avg = currentSum / k;
-            max = Math.max(max, avg );
-            currentSum = 0;
+            System.out.println(Arrays.toString(arr.toArray()));
+            double currentAverage = (double) sum / k;
+            maxAverage = Math.max(maxAverage, currentAverage);
         }
-
-        return 0d;
+        return maxAverage;
     }
 
 
