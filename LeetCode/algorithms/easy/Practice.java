fa206c26671363ea01f266093b58763fafe8ce73
Boula Akladyous <akladyous@gmail.com>
2023-06-18 10:32:33 -0400

    Untracked files on algorithm

--- LeetCode/algorithms/easy/Practice.java
+++ LeetCode/algorithms/easy/Practice.java
@@ -4,20 +4,10 @@ import java.util.*;
 
 public class Practice {
     public static void main(String[] args) {
-        int[] nums1 = {1, 1, 2};
-        int k1 = removeDuplicatesFromSortedArray_26(nums1);
-        System.out.println(k1); // Output: 2
 
-        int[] nums2 = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-        int k2 = removeDuplicatesFromSortedArray_26(nums2);
-        System.out.println(k2); // Output: 5
-
-        int[] nums3 = {1, 2, 3, 3, 4, 4, 5};
-        int k3 = removeDuplicatesFromSortedArray_26(nums3);
-        System.out.println(k3); // Output: 5
     }
 
-    static int removeDuplicatesFromSortedArray_26_1(int[] nums) {
+    static int removeDuplicatesFromSortedArray_26(int[] nums) {
         int left = 0;
         for (int right=0; right < nums.length; right++) {
             /*
@@ -34,17 +24,7 @@ public class Practice {
         return left + 1;
     }
 
-    static int removeDuplicatesFromSortedArray_26(int[] nums) {
-        /*
 
-            0, 0, 1, 1, 1, 2, 2, 3, 3, 4
-        */
-
-        for(int r=nums.length-1, left =r-1; r>= 0; r--) {
-
-        }
-
-    }
 
     static boolean isValid(String s) {
         HashMap<Character, Character> parentheses = new HashMap<>();
