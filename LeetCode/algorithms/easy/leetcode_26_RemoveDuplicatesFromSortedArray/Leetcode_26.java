25640d62cf2e151029aead0bd0c14fb7ad52ea8c
Boula Akladyous <akladyous@gmail.com>
2023-06-18 21:23:15 -0400

    Untracked files on algorithm

--- LeetCode/algorithms/easy/leetcode_26_RemoveDuplicatesFromSortedArray/Leetcode_26.java
+++ LeetCode/algorithms/easy/leetcode_26_RemoveDuplicatesFromSortedArray/Leetcode_26.java
@@ -1,15 +1,27 @@
 package easy.leetcode_26_RemoveDuplicatesFromSortedArray;
+
+import java.util.Arrays;
+
 /*
-Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+Given an integer array nums sorted in non-decreasing order,
+remove the duplicates in-place such that each unique element appears only once.
+The relative order of the elements should be kept the same.
+Then return the number of unique elements in nums.
 
-Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+Consider the number of unique elements of nums to be k, to get accepted,
+you need to do the following things:
 
-Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+Change the array nums such that the first k elements of nums contain
+the unique elements in the order they were present in nums initially.
+The remaining elements of nums are not important as well as the size of nums.
 Return k.
 */
 public class Leetcode_26 {
 
     public static void main(String[] args) {
+        int[] nums0 = {1, 2, 2, 3};
+        int k0 = leetcode_26_removeDuplicatesFromSortedArray(nums0);
+        System.out.println(k0); // Output: 2
 
         int[] nums1 = {1, 1, 2};
         int k1 = leetcode_26_removeDuplicatesFromSortedArray(nums1);
@@ -24,9 +36,13 @@ public class Leetcode_26 {
         int[] nums2 = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
         int k2 = leetcode_26_removeDuplicatesFromSortedArray(nums2);
         System.out.println(k2); // Output: 5
-
-
     }
+    /*
+        [1, 2, 2, 3]
+        [1, 2, 2, 3]
+        [1, 2, 3, 3]
+    */
+
     // it's equivalent to: counts the number of unique elements in the nums array.
     public static int leetcode_26_removeDuplicatesFromSortedArray(int[] nums) {
         int left = 0;
@@ -35,6 +51,7 @@ public class Leetcode_26 {
                 left ++;
                 nums[left] = nums[right];
             }
+            System.out.println(Arrays.toString(nums));
         }
         return left + 1;
     }
--- LeetCode/algorithms/practice/Practice.java
+++ LeetCode/algorithms/practice/Practice.java
@@ -6,11 +6,28 @@ import java.util.*;
 
 public class Practice {
     public static void main(String[] args) {
-
+        int[] nums0 = {1,1,2,2,3};
+        int k0 = leetcode_26_removeDuplicatesFromSortedArray(nums0);
+        System.out.println(k0); // Output: 2
     }
 
-
-
+    static int leetcode_26_removeDuplicatesFromSortedArray(int[] nums) {
+        int left = 0;
+        // 0 1 2 3 4
+        // 1 1 2 2 3
+        // L R
+        // L   R        match
+        //   L R
+        //   L     R
+        //     L   R
+        for (int right = 1; right < nums.length; right ++) {
+            if ( nums[right] > nums[left]) {
+                left ++;
+                nums[left] = nums[right];
+            }
+        }
+        return left + 1;
+    }
 
 }
 
