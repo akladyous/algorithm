e64e6a28aaaa7ba0262dec7145a1240666a0600b
Boula Akladyous <akladyous@gmail.com>
2023-06-20 12:15:51 -0400

    Untracked files on algorithm

--- JavaScript/easy/leetcode_2619_ArrayPrototypeLast.js
+++ JavaScript/easy/leetcode_2619_ArrayPrototypeLast.js
@@ -10,15 +10,3 @@ Array.prototype.last = function () {
     return this[this.length - 1];
   }
 };
-
-async function delayExample() {
-  console.log('Before delay');
-  const millis = 2000; // 2 seconds
-  await new Promise((resolve) => setTimeout(resolve, millis));
-  console.log('After delay');
-  return millis;
-}
-
-delayExample().then((duration) => {
-  console.log(`Delay of ${duration} milliseconds completed.`);
-});
